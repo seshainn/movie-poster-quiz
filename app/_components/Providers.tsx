d2e6b6f3b22aac262d61388b59350829f828d5a6
seshu <seshu.giri@gmail.com>
2023-12-17 23:39:43 +0530

    Fix: trpc client url

--- app/_components/Providers.tsx
+++ app/_components/Providers.tsx
@@ -10,7 +10,7 @@ export const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: 'http://localhost:3000/api/trpc',
+          url: 'https://movie-poster-quiz.vercel.app/api/trpc',
         }),
       ],
     })
